b6a956e76464357ac371b0c8b916274a5a69db3d
Ayush Panta <ayushkpanta@gmail.com>
2024-08-12 13:05:41 -0700

    Initial Commit

---
+++ Wand/WandApp.swift
@@ -0,0 +1,17 @@
+//
+//  WandApp.swift
+//  Wand
+//
+//  Created by Ayush Panta on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
